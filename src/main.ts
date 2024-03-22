3db5274793a9adaa97998beb4c36e32d257887d3
ewi josepha <ewijosepha@gmail.com>
2024-03-22 14:42:45 +0100

    connecting db

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser'
+import * as cors from 'cors'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
+  app.use(cors())
   app.use(cookieParser())
   await app.listen(4000);
 }
