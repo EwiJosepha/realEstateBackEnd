50cee57ab1415682f62fcc9b848e182049061a62
nsamedaisy <nsamedaisy@gmail.com>
2024-04-08 10:43:16 +0100

    agentId missing

--- src/properties/properties.service.ts
+++ src/properties/properties.service.ts
@@ -14,7 +14,7 @@ export class PropertiesService {
 
   async getOneProperty(id: number): Promise<Properties> {
     return this.prisma.properties.findUnique({
-      where: { id: Number(id) },
+      where: { id: id },
       include: {
         agent: true,
       }
